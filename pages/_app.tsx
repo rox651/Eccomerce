c68719a8d5b8b066d9adf0f82bbecaa3d4387c13
Homero José <96498455+rox651@users.noreply.github.com>
2023-01-29 15:02:34 -0500

    si

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,9 @@ import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { AnimatePresence, motion } from "framer-motion";
 import Layout from "@/components/layout/Layout";
 
-const pageAnimations = {
-   initial: {
-      opacity: 0,
-      clipPath: "circle(0% at 0 100%)",
-   },
-   animate: {
-      opacity: 1,
-      clipPath: "circle(141.5% at 0 100%)",
-   },
-   exit: {
-      opacity: 0,
-      clipPath: "circle(0% at 0 100%)",
-   },
-};
-
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
    const queryClient = new QueryClient({
       defaultOptions: {
          queries: {
@@ -34,20 +18,9 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 
    return (
       <QueryClientProvider client={queryClient}>
-         <AnimatePresence mode="wait">
-            <motion.main
-               key={router.route}
-               initial="initial"
-               animate="animate"
-               exit="exit"
-               variants={pageAnimations}
-               transition={{ duration: 0.3 }}
-            >
-               <Layout>
-                  <Component {...pageProps} />
-               </Layout>
-            </motion.main>
-         </AnimatePresence>
+         <Layout>
+            <Component {...pageProps} />
+         </Layout>
          <ReactQueryDevtools />
       </QueryClientProvider>
    );
